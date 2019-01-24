137880fa92b9ead18a601b6c30ff6296785d7011
Louie O'Grady <l.ogrady@gmail.com>
2019-01-24 17:50:33 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,5 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
-def loop_iterator(number_of_times)
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
@@ -8,9 +7,15 @@ def loop_iterator(number_of_times)
   #   correct number of times?
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
+
+def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  loop do
+    puts phrase
+    break if number_of_times > 7
+  end
 end
+
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
