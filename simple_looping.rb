9d1d4b83639e6cd9d475f779a65b3f28f1ba50b2
Louie O'Grady <l.ogrady@gmail.com>
2019-01-24 18:15:15 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -22,8 +22,8 @@ end
 
 def times_iterator(number_of_times)
   number_of_times.times do
-    phrase = "Welcome to Flatiron School's Web Development Course!"
-end
+    puts "Welcome to Flatiron School's Web Development Course!"
+  end
 end
 
 def while_iterator(number_of_times)
